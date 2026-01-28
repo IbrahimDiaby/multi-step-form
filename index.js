f7e78665b84ab09765cf2611b806b18f403cb5ac
Ibrahim Diaby <d.ibrahim2016@gmail.com>
2026-01-29 00:11:05 +0100

    [ADD] : Slide Animation

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
   const next = document.querySelector("form .btn");
+  const slideSteps = document.querySelectorAll(".step-slide");
 
   next.addEventListener("click", (e) => {
-    e.stopPropagation();
     e.preventDefault();
+
+    slideSteps.forEach(slide => {
+      const width = slide.getBoundingClientRect().width;
+      slide.style.transform = `translateX(-${width}px)`;
+    });
   });
 });
