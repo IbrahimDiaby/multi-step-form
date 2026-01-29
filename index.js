f995fc1ab88276397d6f6f752154975cc936337a
Ibrahim Diaby <d.ibrahim2016@gmail.com>
2026-01-29 13:36:13 +0100

    [FIX] : Required error msg

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const nextBtns = document.querySelectorAll("form .btn.next");
   const slideSteps = document.querySelectorAll(".step-slide");
   const steps = document.querySelectorAll(".step");
+  const labelsContainer = document.querySelectorAll(".label-container");
 
   const updateSteps = (activeIndex) => {
     steps.forEach((step, idx) => {
-      step.querySelector(".step-index")
+      step
+        .querySelector(".step-index")
         .classList.toggle("active", idx === activeIndex);
     });
-  }
+  };
 
   const translateSlides = (targetIndex) => {
     slideSteps.forEach((slide) => {
       const width = slide.getBoundingClientRect().width;
       slide.style.transform = `translateX(-${width * targetIndex}px)`;
     });
-  }
+  };
 
   previousBtns.forEach((btn, index) => {
     btn.addEventListener("click", (e) => {
@@ -31,8 +33,49 @@ window.addEventListener("DOMContentLoaded", () => {
     btn.addEventListener("click", (e) => {
       e.preventDefault();
 
-      updateSteps(index + 1);
-      translateSlides(index + 1);
+      labelsContainer.forEach((label) => {
+        label.querySelector("span.required-error").classList.remove("visible");
+      })
+
+      if (index === 0) {
+        // Personal Info
+        const inputName = document.querySelector(
+          "form#personalInfo input#name",
+        );
+        const inputEmail = document.querySelector(
+          "form#personalInfo input#email",
+        );
+        const inputTel = document.querySelector("form#personalInfo input#tel");
+
+        if (inputName.value === "") {
+          labelsContainer[0]
+            .querySelector("span.required-error")
+            .classList.toggle("visible");
+        }
+
+        if (inputEmail.value === "") {;
+          labelsContainer[1]
+            .querySelector("span.required-error")
+            .classList.toggle("visible");
+        }
+
+        if (inputTel.value === "") {
+          labelsContainer[2]
+            .querySelector("span.required-error")
+            .classList.toggle("visible");
+        }
+
+        if (
+          !(
+            inputName.value === "" ||
+            inputEmail.value === "" ||
+            inputTel.value === ""
+          )
+        ) {
+          updateSteps(index + 1);
+          translateSlides(index + 1);
+        }
+      }
     });
   });
 });
